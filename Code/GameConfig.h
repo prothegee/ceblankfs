39d19d06394269b518153f921dd86b6441ed9b9c
prothegee <prothegee@outlook.com>
2022-07-20 08:47:21 +0700

    checkpoint for pch

--- Code/GameConfig.h
+++ Code/GameConfig.h
@@ -1,29 +1,5 @@
-// act as this project game config & may as pre compile header
 #pragma once
-
-#include <array>
-#include <numeric>
-
-
-#include "StdAfx.h"
-
-
-#include <CryEntitySystem/IEntityComponent.h>
-
-
-#include <CryCore/StaticInstanceList.h>
-#include <CrySchematyc/Env/IEnvRegistrar.h>
-#include <CrySchematyc/Env/Elements/EnvComponent.h>
-
-
-#include <DefaultComponents/Cameras/CameraComponent.h>
-#include <DefaultComponents/Input/InputComponent.h>
-#include <DefaultComponents/Physics/CharacterControllerComponent.h>
-#include <DefaultComponents/Geometry/AdvancedAnimationComponent.h>
-#include <DefaultComponents/Geometry/AlembicComponent.h>
-#include <DefaultComponents/Geometry/AnimatedMeshComponent.h>
-#include <DefaultComponents/Geometry/BaseMeshComponent.h>
-#include <DefaultComponents/Geometry/StaticMeshComponent.h>
+#include "pch_ceblankfs.h"
 
 
 namespace ceblankfs
---
+++ Code/pch_ceblankfs.h
@@ -0,0 +1,31 @@
+/**
+ * @file pch_ceblankfs.h
+ * @brief precompiler header for ceblankfs project
+ * 
+ * @note lazy mode to include header/s if you may
+ * 
+ */
+#pragma once
+#include <array>
+#include <numeric>
+
+
+#include "StdAfx.h"
+
+
+#include <CryEntitySystem/IEntityComponent.h>
+
+
+#include <CryCore/StaticInstanceList.h>
+#include <CrySchematyc/Env/IEnvRegistrar.h>
+#include <CrySchematyc/Env/Elements/EnvComponent.h>
+
+
+#include <DefaultComponents/Cameras/CameraComponent.h>
+#include <DefaultComponents/Input/InputComponent.h>
+#include <DefaultComponents/Physics/CharacterControllerComponent.h>
+#include <DefaultComponents/Geometry/AdvancedAnimationComponent.h>
+#include <DefaultComponents/Geometry/AlembicComponent.h>
+#include <DefaultComponents/Geometry/AnimatedMeshComponent.h>
+#include <DefaultComponents/Geometry/BaseMeshComponent.h>
+#include <DefaultComponents/Geometry/StaticMeshComponent.h>
